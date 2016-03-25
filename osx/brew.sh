95b0b016c56f3a459903d4a1a068f2c19fc3d432
André Carvalho <asantostc@gmail.com>
2016-03-25 18:45:27 -0300

    Adds brew instalation scripts

---
+++ osx/brew.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+# Install Homebrew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/cask
+
+brew update
+brew upgrade
+
+apps=(
+	vim --override-system-vi --with-lua
+	git
+	bash-completion2
+	wrk
+	tmux
+	tig
+	wget
+	brew-cask
+)
+
+brew install "${apps[@]}"
+
+# Install Caskroom and packages
+
+brew tap caskroom/versions
+
+apps=(
+	atom
+	dropbox
+	firefox
+	google-chrome
+	spotify
+)
+
+brew cask install "${apps[@]}$"
