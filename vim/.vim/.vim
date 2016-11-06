5e1ca12fff31ac819d1454e7a0459d5517c8975f
André Carvalho <asantostc@gmail.com>
2016-11-05 22:13:30 -0200

    clean unused stuff

--- vim/.vim/.vim
+++
@@ -1,1 +0,0 @@
-/Users/andrestc/dotfiles/vim/.vim
--- vim/.vimrc.go
+++
@@ -1,9 +0,0 @@
-set runtimepath^=~/.vim_go_runtime
-source ~/.vim_go_runtime/vimrc/basic.vim
-source ~/.vim_go_runtime/vimrc/filetypes.vim
-source ~/.vim_go_runtime/vimrc/plugins.vim
-source ~/.vim_go_runtime/vimrc/extended.vim
-try
-source ~/.vim_go_runtime/custom_config.vim
-catch
-endtry
